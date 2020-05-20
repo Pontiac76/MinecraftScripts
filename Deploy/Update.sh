f008e6f7d6fa599fe24fadd6c980603b65e7a640
Pontiac76 <pontiac76@gmail.com>
2020-05-20 03:50:15 -0400

    Create Update.sh

---
+++ Deploy/Update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+## Download the most recent Deploy script from GitHub
+wget "https://raw.githubusercontent.com/Pontiac76/MinecraftScripts/master/Deploy/DeployMC.sh" -o DeployMC.sh
+
+## Run the script
+sh DeployMC.sh
